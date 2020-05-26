03c750825edcb571703c2bdc5e090958b599530b
FortyNina <nsd286@nyu.edu>
2020-05-26 12:36:04 -0500

    i am a little confused

---
+++ Assets/Backend/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+
+	//in seconds
+	public float timeLimit = 180f;
+
+	[SerializeField]
+	private TextMeshProUGUI _timeDisplay;
+
+	private float _timer;
+
+	void Start(){
+		_timer = timeLimit;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer -= Time.deltaTime;
+
+        Debug.Log((int)(_timer / 60));
+
+
+    }
+}
