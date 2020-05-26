ee766883410d9017be7ec87edba38e6987f79317
FortyNina <nsd286@nyu.edu>
2020-05-26 03:47:42 -0500

    add simple menu and hook up candidate stat meters in testui script

---
+++ Assets/UI/Scripts/CandidateStatMeter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Place on a Candidate Stat Meter (ex: Wealth). 
+
+public class CandidateStatMeter : MonoBehaviour
+{
+	[SerializeField]
+	private Image _positiveMeter;
+
+	[SerializeField]
+	private Image _negativeMeter;
+
+	[SerializeField]
+	private GameObject _negativeAxis;
+
+	[SerializeField]
+	private GameObject _positiveAxis;
+
+
+    public void SetCandidateStats(int level, int max){
+    	
+    	_positiveAxis.SetActive(false);
+    	_negativeAxis.SetActive(false);
+
+    	_positiveMeter.fillAmount = 0;
+    	_negativeMeter.fillAmount = 0;
+
+    	if(level < 0){
+    		_negativeAxis.SetActive(true);
+    		_negativeMeter.fillAmount = ((float)level / (float)max);
+    	}
+    	else{
+    		_positiveAxis.SetActive(true);
+    		_positiveMeter.fillAmount = ((float)level / (float)max);
+    	}
+    }
+}
---
+++ Assets/UI/Scripts/CultStatMeter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CultStatMeter : MonoBehaviour
+{
+
+	[SerializeField]
+	private Image _meter;
+    
+	public void SetCultStats(int level, int max){
+		_meter.fillAmount = (float)level / (float)max;
+	}
+
+
+
+}
