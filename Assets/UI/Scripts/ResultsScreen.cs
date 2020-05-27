38e17f464accb4a94573a8c3f9d187df985494e5
FortyNina <nsd286@nyu.edu>
2020-05-27 12:28:56 -0500

    add more information to the results screen

--- Assets/UI/Scripts/ResultsScreen.cs
+++ Assets/UI/Scripts/ResultsScreen.cs
@@ -8,74 +8,100 @@ using TMPro;
 
 public class ResultsScreen : MonoBehaviour
 {
-	public float animationInterval = .02f;
+	public float animationInterval = .08f;
 
 	[SerializeField]
-	private GameObject _wealthMeter;
+	private Image _wealthMeter;
 
 	[SerializeField]
-	private GameObject _reputationMeter;
+	private Image _reputationMeter;
 
 	[SerializeField]
-	private GameObject _loyaltyMeter;
+	private Image _loyaltyMeter;
 
 	[SerializeField]
 	private Button _playAgainButton;
 
-	private int _wealthVal = 30;
-	private int _reputationVal = 55;
-	private int _loyaltyVal = 20;
+	[SerializeField]
+	private TextMeshProUGUI _cultRanking;
+
+	[SerializeField]
+	private TextMeshProUGUI _cultEvaluation;
+
+	private int _wealthVal = 90;
+	private int _reputationVal = 75;
+	private int _loyaltyVal = 70;
+	private int _valMax = 100;
+
+	private bool _evaluate;
 
 
     // Start is called before the first frame update
     void OnEnable(){
 
-    	StartCoroutine(MeterDisplay());
     	_playAgainButton.onClick.AddListener(RestartGame);
 
+    	//enter welath, reputation , and lyalty vals here
+
+    	StartCoroutine(MeterDisplay(_wealthMeter, _wealthVal));
+    	StartCoroutine(MeterDisplay(_reputationMeter, _reputationVal));
+    	StartCoroutine(MeterDisplay(_loyaltyMeter, _loyaltyVal));
+
     }
 
+    void Update()
+    {
+    	
+    }
 
-
-    IEnumerator MeterDisplay(){
-
+    IEnumerator MeterDisplay(Image meter, int val) {
     	float currentFill = 0;
-    	Image wealthIm = _wealthMeter.GetComponentInChildren<Image>();
-    	_wealthMeter.SetActive(true);
-    	wealthIm.fillAmount = 0;
+    	meter.fillAmount = 0;
 
-    	while(currentFill < (float)_wealthVal / (100)){
+    	while(currentFill < (float)val / (_valMax)){
     		yield return new WaitForSeconds(animationInterval);
     		currentFill += .01f;
-    		wealthIm.fillAmount = currentFill;
+    		meter.fillAmount = currentFill;
     	}
+    	if(!_evaluate){
+    		EvaluationDisplay();
+    		_evaluate = true;
 
-
-
-    	currentFill = 0;
-    	Image reputationIm = _reputationMeter.GetComponentInChildren<Image>();
-    	_reputationMeter.SetActive(true);
-    	reputationIm.fillAmount = 0;
-
-    	while(currentFill < (float)_reputationVal / (100)){
-    		yield return new WaitForSeconds(animationInterval);
-    		currentFill += .01f;
-    		reputationIm.fillAmount = currentFill;
-    	}
-
-
-    	currentFill = 0;
-    	Image loyaltyIm = _loyaltyMeter.GetComponentInChildren<Image>();
-    	_loyaltyMeter.SetActive(true);
-    	loyaltyIm.fillAmount = 0;
-
-    	while(currentFill < (float)_loyaltyVal / (100)){
-    		yield return new WaitForSeconds(animationInterval);
-    		currentFill += .01f;
-    		loyaltyIm.fillAmount = currentFill;
     	}
 
     }
+
+
+    private void EvaluationDisplay(){
+
+    	float score = ((float)_wealthVal + (float)_reputationVal + (float)_loyaltyVal) / (float)(_valMax * 3);
+
+    	Debug.Log(score);
+
+    	_cultRanking.gameObject.SetActive(true);
+    	_cultEvaluation.gameObject.SetActive(true);
+
+    	if(score < .33f){
+    		_cultRanking.text = "Cult Ranking: Pathetic";
+    		_cultEvaluation.text = "Your cult is pathetic. Your cult members will probably revolt against your authority at some point, and then you'll have to get a real job.";
+    	}
+
+    	else if(score < .66f){
+    		_cultRanking.text = "Cult Ranking: Mediocre";
+    		_cultEvaluation.text = "Your cult is mediocre. If you're lucky, maybe someone will make a podcast about it, but it's definitely not going down in history as one of the 'greats'.";
+
+    	}
+
+    	else{
+    		_cultRanking.text = "Cult Ranking: Legendary";
+    		_cultEvaluation.text = "Your cult is legendary. Your members are so brainwashed that they'll still speak highly of you in the documentaries that are made after your death.";
+
+    	}
+
+
+    }
+
+
 
     private void RestartGame(){
     	SceneManager.LoadScene("GameScene");
